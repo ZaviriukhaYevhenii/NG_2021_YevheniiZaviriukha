228259ca97cdac3fb602a53b145e57fa340efe70
Yevhenii Zaviriukha <jewgendrakon@gmail.com>
2021-07-08 21:28:46 +0300

    code for up to 2 task

---
+++ git/Task2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numeric_one;
+    int numeric_two;
+    cout << "Vvedite pervoe chislo: ";
+    cin >> numeric_one;
+    cout << "Vvedite vtoroe chislo: ";
+    cin >> numeric_two;
+    cout << "Summa " << numeric_one + numeric_two << endl;
+    system("pause");
+}
