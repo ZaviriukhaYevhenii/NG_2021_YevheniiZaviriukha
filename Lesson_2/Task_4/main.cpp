b9c95f2a450692de120547f72b6bedc6eb6a5b6f
Yevhenii Zaviriukha <jewgendrakon@gmail.com>
2021-09-20 22:57:13 +0300

    half a square was created in this task

---
+++ Lesson_2/Task_4/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Square size: ";
+    cin >> size;
+
+    for (int row = 0; row <= size; row++){
+        for(int column = 0; column < row; column++)
+        cout << "*";
+    cout << endl;
+}
+cout << endl;
+for (int line = size; line > 0; line--)
+    {
+        for (int asterisk = 0; line > asterisk; asterisk++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int line = size; line > 0; line--)
+    {
+        for (int space = 0; space < size - line; space++)
+        {
+            cout << " ";
+        }
+        for (int asterisk = 0; line > asterisk; asterisk++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int line = 0; line <= size; line++)
+    {
+        for (int space = 0; space < size - line; space++)
+        {
+            cout << " ";
+        }
+        for (int asterisk = 0; line > asterisk; asterisk++)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
