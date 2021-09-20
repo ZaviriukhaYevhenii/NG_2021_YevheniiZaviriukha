09986675561d267dd273b21117e572494c98b5d0
Yevhenii Zaviriukha <jewgendrakon@gmail.com>
2021-09-20 23:10:40 +0300

    half a square was created in this task

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -9,41 +9,39 @@ int main()
     cin >> size;
 
     for (int row = 0; row <= size; row++){
-        for(int column = 0; column < row; column++)
+        for(int column = 0; column < row; column++){
         cout << "*";
+        }
     cout << endl;
 }
 cout << endl;
-for (int line = size; line > 0; line--)
-    {
-        for (int asterisk = 0; line > asterisk; asterisk++)
-        {
+
+    for (int line = size; line > 0; line--){
+        for (int asterisk = 0; line > asterisk; asterisk++){
             cout << "*";
         }
         cout << endl;
     }
     cout << endl;
-    for (int line = size; line > 0; line--)
-    {
-        for (int space = 0; space < size - line; space++)
-        {
+
+    for (int line = size; line > 0; line--){
+        for (int space = 0; space < size - line; space++){
             cout << " ";
         }
-        for (int asterisk = 0; line > asterisk; asterisk++)
-        {
+        for (int asterisk = 0; line > asterisk; asterisk++){
             cout << "*";
         }
         cout << endl;
     }
-    for (int line = 0; line <= size; line++)
-    {
-        for (int space = 0; space < size - line; space++)
-        {
+
+    for (int line = 0; line <= size; line++){
+        for (int space = 0; space < size - line; space++){
             cout << " ";
         }
         for (int asterisk = 0; line > asterisk; asterisk++)
             cout << "*";
         cout << endl;
     }
+    system("pause");
     return 0;
 }
